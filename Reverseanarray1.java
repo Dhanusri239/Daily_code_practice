1fd57f6036b4c128f06d0b21972160b166947001
Dhanusri239 <sdhanu946@gmail.com>
2025-08-09 12:03:29 +0530

    Method 1 of reverse an array prog is added

---
+++ Reverseanarray1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Reverseanarray1 {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int s=0;
+        int e=n-1;
+        while(s<e){
+            int temp=arr[s];
+           arr[s]=arr[e];
+           arr[e]=temp;
+           s++;
+           e--;
+        }
+        System.out.println("The Reversed array is:"+Arrays.toString(arr));
+        sc.close();
+    }
+}
