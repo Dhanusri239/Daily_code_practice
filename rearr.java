c0f70c3cc82117c9fda1961178b8cd3ea5a37a73
Dhanusri239 <sdhanu946@gmail.com>
2025-09-12 12:42:36 +0530

    added

---
+++ rearr.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class rearr {
+         public static void ra(int k,int arr[]){
+            int n=arr.length;
+            k=k%n;
+            re(arr,0,k-1);
+            re(arr,k,n-1);
+            re(arr,0,n-1);
+            System.out.println(Arrays.toString(arr));
+         }
+         public static void re(int arr[],int a,int b ){
+            while(a<b){
+                int t=arr[a];
+                arr[a]=arr[b];
+                arr[b]=t;
+                a++;
+                b--;
+            }
+         }
+   public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int arr[]=new int[n];
+    for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+    }
+    int k=sc.nextInt();
+    ra(k,arr);
+    
+   }
+}
